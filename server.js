d58e49c18eda8271acd067297ac89b77921bf0ec
Gustavo361 <gustavofaustino361@gmail.com>
2024-02-17 14:01:30 -0300

    logout fixing route

--- server.js
+++ server.js
@@ -78,9 +78,6 @@ passport.deserializeUser(function (id, done) {
 
 app.get('/logout', isAuthenticated, (req, res) => {
     req.logout()
-    // Adicione qualquer lógica adicional de limpeza de sessão ou usuário
-
-    // Responda à solicitação Fetch com sucesso
     return res.status(200).json({ message: 'Logout bem-sucedido.', redirectRoute: 'https://easy-bank-ui.onrender.com'})
 
 })
