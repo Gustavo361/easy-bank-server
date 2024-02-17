2393af649618b3ab5ec3017d381df9d59bb498e6
Gustavo361 <gustavofaustino361@gmail.com>
2024-02-17 15:02:08 -0300

    fixing route

--- server.js
+++ server.js
@@ -77,9 +77,9 @@ passport.deserializeUser(function (id, done) {
 })
 
 app.get('/logout', checkAuthentication, (req, res) => {
-    req.logout()
+    // req.logout()
     // return res.status(200).json({ message: 'Logout bem-sucedido.', redirectRoute: 'https://easy-bank-ui.onrender.com'})
-    res.redirect('https://easy-bank-ui.onrender.com')
+    res.redirect('https://easy-bank-ui.onrender.com/index.html')
 })
 
 mongoose.connect(dbconfig.databaseConnectionString)
