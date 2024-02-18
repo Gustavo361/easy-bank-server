1c58c6d3cf668c01ada4c82be6e43aff0de84535
Gustavo361 <gustavofaustino361@gmail.com>
2024-02-18 13:36:27 -0300

    .

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ passport.deserializeUser(function (id, done) {
     })
 })
 
-app.get('https://easy-bank-server.onrender.com/logout', (req, res) => {
+app.get('/logout', (req, res) => {
     req.logout()
     res.redirect('https://easy-bank-ui.onrender.com')
 })
