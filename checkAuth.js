2faf28562a61659d43621d8c7c53d22e58f5468e
Gustavo361 <gustavofaustino361@gmail.com>
2025-05-26 16:34:33 -0300

    auth updates

--- checkAuth.js
+++ checkAuth.js
@@ -1,18 +1,30 @@
+// function checkAuthentication(req, res, next) {
+//     if (req.isAuthenticated()) {
+//         // Se autenticado e tentando acessar login/cadastro, redireciona
+//         if (req.path === '/login' || req.path === '/create-account') {
+//             return res.redirect('https://easy-bank-ui.onrender.com/initial');
+//         }
+//         return next();
+//     } else {
+//         // Se NÃO autenticado e tentando acessar rotas protegidas
+//         if (req.path === '/' || req.path === '/initial' || req.path === '/create-account') {
+//             return res.redirect('https://easy-bank-ui.onrender.com/login');
+//         }
+//         return next();
+//     }
+// }
+
+// module.exports = checkAuthentication
+
+
 function checkAuthentication(req, res, next) {
     if (req.isAuthenticated()) {
-        // Se autenticado e tentando acessar login/cadastro, redireciona
-        if (req.path === '/login' || req.path === '/create-account') {
-            return res.redirect('https://easy-bank-ui.onrender.com/initial');
-        }
+        // Permite acesso a rotas protegidas
         return next();
     } else {
-        // Se NÃO autenticado e tentando acessar rotas protegidas
-        if (req.path === '/' || req.path === '/initial' || req.path === '/create-account') {
-            return res.redirect('https://easy-bank-ui.onrender.com/login');
-        }
-        return next();
+        // Redireciona para login se tentar acessar rota protegida
+        return res.redirect('https://easy-bank-ui.onrender.com/login');
     }
 }
 
 module.exports = checkAuthentication
-
